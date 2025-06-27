3fe1a5a007f990307bc519a7ca384c2a32222bfa
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2025-06-27 17:16:55 +0200

    Update 02_WT_Pseudotime.R

--- R/3_scRNA-seq/02_WT_Pseudotime.R
+++ R/3_scRNA-seq/02_WT_Pseudotime.R
@@ -24,7 +24,7 @@ rm(list=ls(all.names=TRUE))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PROJECT INFO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ################################################################################
 
-PATH <- 'C:/Users/E15639P/Desktop/GitHub_NF_dev_ILC'
+PATH <- 'C:/Users/E15639P/Desktop/NFIL3_dev_ILC'
 
 ################################################################################
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -33,7 +33,7 @@ PATH <- 'C:/Users/E15639P/Desktop/GitHub_NF_dev_ILC'
 setwd(PATH)
 
 # Load Packages and custom functions
-source('C:/Users/E15639P/NFIL3_dev_ILC/Custom_Functions.R')
+source('C:/Users/E15639P/Desktop/NFIL3_dev_ILC/Custom_Functions.R')
 suppressPackageStartupMessages(library(Seurat))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(slingshot))
