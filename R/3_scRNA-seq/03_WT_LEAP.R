7502c2aad359be52059304f5f576c5e6081e0e20
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2025-06-27 17:15:11 +0200

    Update 03_WT_LEAP.R

--- R/3_scRNA-seq/03_WT_LEAP.R
+++ R/3_scRNA-seq/03_WT_LEAP.R
@@ -22,7 +22,7 @@ rm(list=ls(all.names=TRUE))
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ PROJECT INFO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 ################################################################################
 
-PATH <- 'C:/Users/E15639P/Desktop/GitHub_NF_dev_ILC'
+PATH <- 'C:/Users/E15639P/Desktop/NFIL3_dev_ILC'
 
 ################################################################################
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -31,7 +31,7 @@ PATH <- 'C:/Users/E15639P/Desktop/GitHub_NF_dev_ILC'
 setwd(PATH)
 
 # Load Packages and custom functions
-source('C:/Users/E15639P/Desktop/GitHub_NF_dev_ILC/Scripts/Custom_Functions.R')
+source('C:/Users/E15639P/Desktop/NFIL3_dev_ILC/Custom_Functions.R')
 suppressPackageStartupMessages(library(Seurat))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(igraph))
@@ -61,7 +61,7 @@ data     <- readRDS(paste0(PATH_SAVE, '/5_Pseudotime.rds'))
 ## LEAP MATRIX PREPARATION -----------------------------------------------------
 #===============================================================================
 
-# Generate matrix and index for ILC trajectory
+## Generate matrix and index for ILC trajectory
 result_S1 <- LeapMatrix(data, trajectory = 'Slingshot_1', suffix = 'WT',  
                         min.cells = 50, write = T, 
                         dir = paste0(PATH_SAVE, '/LEAP'), slot = 'data')
@@ -73,14 +73,14 @@ matrix <- as.data.frame(result_S1[1])
 #    READY TO USE GENERATED TABLE FOR LEAP ANALYSIS ON CALCULATION CLUSTER     #
 ################################################################################
 #
-# MAC_results <- MAC_counter(data = matrix, max_lag_prop = 1/3, 
+# MAC_results <- MAC_counter(data = as.matrix(matrix), max_lag_prop = 1/3, 
 #                            MAC_cutoff = 0.1, lag_matrix = TRUE, 
 #                            file_name = 'WT')
 # write.table(x = MAC_results, 
 #             paste0(PATH, '/', 'MAC_results_', SUFFIX, '.txt'), 
 #             row.names = FALSE)
 #
-# MAC_perm(data = matrix, MACs_observ = MAC_matrix, num_perms = 100, 
+# MAC_perm(data = as.matrix(matrix), MACs_observ = MAC_matrix, num_perms = 100, 
 #          max_lag_prop = 1/3, FDR_cutoffs = 401, perm_file_name = 'WT')
 #
 ################################################################################
@@ -107,8 +107,10 @@ Annotate    <- Annotate[abs(Annotate$Correlation) > perm_cutoff,]
 
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 # Save filtered interactions
-write.table(Annotate, paste0(PATH_SAVE, '/LEAP/results_indexed_filtered.txt'),
-            row.names = F, col.names = T, quote = F)
+#write.table(Annotate, paste0(PATH_SAVE, '/LEAP/results_indexed_filtered.txt'),
+#            row.names = F, col.names = T, quote = F)
+Annotate <- read.table(paste0(PATH_SAVE, '/LEAP/results_indexed_filtered.txt'),
+                       header = T)
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
@@ -117,11 +119,7 @@ write.table(Annotate, paste0(PATH_SAVE, '/LEAP/results_indexed_filtered.txt'),
 ## LEAP RESULTS ANALYSIS -------------------------------------------------------
 #===============================================================================
 
-Annotate <- read.table(paste0(PATH_SAVE, '/LEAP/results_indexed_filtered.txt'), 
-                       header = T)
 table    <- OrderMatrix(data, 'Slingshot_1', min.cells = 50, slot = 'scaledata')
-
-
 NF       <- Annotate[Annotate$gene_row %in% 'Nfil3' & Annotate$Lag > 0,]
 
 
@@ -131,48 +129,6 @@ write.table(NF, paste0(PATH_SAVE, '/LEAP/TABLE_Nfil3_Candidates.txt'),
             quote = F, row.names = F, col.names = T)
 write.table(NF$gene_col, paste0(PATH_SAVE, '/LEAP/Nfil3_Candidates.txt'), 
             quote = F, row.names = F, col.names = F)
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-
-
-## CROSSING BULK RESULTS -------------------------------------------------------
-Bulk_UP   <- read.table(
-  paste0(PATH, '/Saves/Bulk_RNA-seq/Nfil3/Candidates_UP.txt'))[,1]
-Bulk_DOWN <- read.table(
-  paste0(PATH, '/Saves/Bulk_RNA-seq/Nfil3/Candidates_DOWN.txt'))[,1]
-
-NF_UP   <- NF$gene_col[NF$Correlation > 0]
-NF_DOWN <- NF$gene_col[NF$Correlation < 0]
-
-Cross.UP   <- unique(NF_UP[NF_UP %in% Bulk_UP])
-Cross.DOWN <- unique(NF_DOWN[NF_DOWN %in% Bulk_DOWN])
-
-# Draw LEAP candidates
-for(g in c(NF_UP, NF_DOWN)){
-  DrawExpr(table, feature.list = g, bin.number = 12, 
-           by.order = F, compare.with = 'Nfil3',
-           scale = T, scale.method = 0, 
-           col = ColorBlind[c(2,8)],
-           write = T, dir = paste0(PATH_FIG, '/Curves/Targets'))
-}
-
-# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-# Save candidate gene lists
-write.table(NF_UP, 
-            paste0(PATH_SAVE, '/LEAP/Candidates_LEAP_UP.txt'), 
-            row.names = F, col.names = F, quote = F)
-write.table(NF_DOWN, 
-            paste0(PATH_SAVE, '/LEAP/Candidates_LEAP_DOWN.txt'), 
-            row.names = F, col.names = F, quote = F)
-
-write.table(Cross.UP, 
-            paste0(PATH, '/Saves/Candidates_Cross_UP.txt'), 
-            row.names = F, col.names = F, quote = F)
-write.table(Cross.DOWN, 
-            paste0(PATH, '/Saves/Candidates_Cross_DOWN.txt'), 
-            row.names = F, col.names = F, quote = F)
-write.table(c(Cross.UP, Cross.DOWN), 
-            paste0(PATH, '/Saves/Candidates_Cross_All.txt'), 
-            row.names = F, col.names = F, quote = F)
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
@@ -206,9 +162,10 @@ edges <- data.frame(From   = Network$gene_row,
                     Neglag = 1.5-as.numeric(Network$Lag/max(Network$Lag))) 
 
 graph  <- graph_from_data_frame(edges, directed = T, vertices = nodes)
-plot19 <- plot(graph, layout = layout.fruchterman.reingold, vertex.size = 45,
-               edge.arrow.size = 0.5, vertex.label=Genes, 
-               vertex.color = ColorBlind[3], vertex.label.color='black',)
+plot(graph, layout = layout.fruchterman.reingold, vertex.size = 45,
+     edge.arrow.size = 0.5, vertex.label=Genes, 
+     vertex.color = ColorBlind[3], vertex.label.color='black',)
+plot19 <- recordPlot()
 writePlot(plot19, PATH_FIG)
 
 
@@ -220,3 +177,9 @@ write.table(Network, paste0(PATH_SAVE, '/LEAP/Supplementary_Table_2.txt'),
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 
+################################################################################
+#      READY FOR DEEPER ANALYSIS OF GENE REGULATORY NETWORK USING GEPHI        #
+################################################################################
+
+
+                                                                                      
